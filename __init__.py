0122e270a28922adfeff6e55cc1803aea9ba1709
CroUsaP <34753719+CroUsaP@users.noreply.github.com>
2019-04-09 01:08:04 +0300

    remove

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import discord, random, os, asyncio
-from discord.ext import commands
-from discord.ext.commands import Bot
-from datetime import datetime
-from pytz import timezone
-colour = 0x39d0d6
-link = "https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8"
-token = open("token.txt", 'r').read()
-adm_help = ["info", "say"]
